c8401587032abba01f057622b7cfac9f209fb9fd
HAYASHI Kentaro <kenhys@gmail.com>
2014-11-09 00:22:43 +0900

    Add control command

--- lib/dcs/debian.rb
+++ lib/dcs/debian.rb
@@ -1,7 +1,27 @@
+require "thor"
+require "open-uri"
+require "nokogiri"
 require "dcs/debian/version"
+require "dcs/debian/helper"
 
 module Dcs
   module Debian
     # Your code goes here...
+
+    class Command < Thor
+
+      include Dcs::Debian::Helper
+
+      desc "control [KEYWORDS]", ""
+      def control(arg)
+
+        puts arg
+
+        pagination("control", arg) do |context|
+          p context
+        end
+      end
+
+    end
   end
 end
